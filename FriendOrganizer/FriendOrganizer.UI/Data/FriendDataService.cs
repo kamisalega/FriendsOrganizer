efa70908c9414ef4c43c28f312e6325b3b26b236
Kamil <kamilsalega@gmail.com>
2020-07-05 22:00:34 +0200

    KER-001 - Planowanie warstwy interfejsu użytkownika

---
+++ FriendOrganizer/FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,20 @@
+﻿using FriendOrganizer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService
+    {
+        public IEnumerable<Friend> GetAll()
+        {
+            yield return new Friend { FirstName = "Jan", LastName = "Kowalski" };
+            yield return new Friend { FirstName = "Franciszek", LastName = "Sztacheta" };
+            yield return new Friend { FirstName = "Julia", LastName = "Niedobra" };
+            yield return new Friend { FirstName = "Krzysztof", LastName = "Jakiśtam" };
+        }
+    }
+}
