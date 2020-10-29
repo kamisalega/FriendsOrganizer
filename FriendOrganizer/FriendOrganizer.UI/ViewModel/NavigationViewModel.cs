614aa01e750595fb5a867d5106d4cac586571d78
Kamil <kamilsalega@gmail.com>
2020-10-29 20:30:54 +0100

    KER-004 - Komunikowanie się między modelami widoku

--- FriendOrganizer/FriendOrganizer.UI/ViewModel/NavigationViewModel.cs
+++ FriendOrganizer/FriendOrganizer.UI/ViewModel/NavigationViewModel.cs
@@ -2,20 +2,42 @@
 using FriendOrganizer.UI.Data;
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
+using FriendOrganizer.UI.Event;
+using Prism.Events;
 
 namespace FriendOrganizer.UI.ViewModel
 {
-    public class NavigationViewModel : INavigationViewModel
+    public class NavigationViewModel : ViewModelBase, INavigationViewModel
     {
         private IFriendLookupDataService _friendLookupDataService;
+        private IEventAggregator _eventAggregator;
+        private LookupItem _selectedFriend;
 
-        public ObservableCollection<LookupItem> Friends { get; }
-
-        public NavigationViewModel(IFriendLookupDataService friendLookupDataService)
+        public NavigationViewModel(IFriendLookupDataService friendLookupDataService, IEventAggregator eventAggregator)
         {
             _friendLookupDataService = friendLookupDataService;
+            _eventAggregator = eventAggregator;
             Friends = new ObservableCollection<LookupItem>();
         }
+
+        public ObservableCollection<LookupItem> Friends { get; }
+     
+
+        public LookupItem SelectedFriend
+        {
+            get { return _selectedFriend; }
+            set
+            {
+                _selectedFriend = value;
+                OnPropertyChanged();
+                if (_selectedFriend != null)
+                {
+                    _eventAggregator.GetEvent<OpenFriendDetailViewEvent>()
+                        .Publish(_selectedFriend.Id);
+                }
+            }
+        }
+
 
         public async Task LoadAsync()
         {
@@ -26,5 +48,7 @@ namespace FriendOrganizer.UI.ViewModel
                 Friends.Add(item);
             }
         }
+
     }
 }
+ 
