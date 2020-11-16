083ab884b0335d86fd701e84b4e38698121c7d10
Kamil <kamilsalega@gmail.com>
2020-11-16 06:04:55 +0100

    KER-008 - Dodawanie i usuwanie jednostek

--- FriendOrganizer/FriendOrganizer.Model/Properties/AssemblyInfo.cs
+++ FriendOrganizer/FriendOrganizer.Model/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FriendOrganizer.Model")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("abd3b1f8-1aec-4aae-bfc8-cb029ea8f4d2")]
+[assembly: Guid("3943529c-dd85-4ec1-908e-93409b052684")]
 
 // Version information for an assembly consists of the following four values:
 //
--- FriendOrganizer/FriendOrganizer.UI/View/Services/MessageDialogService.cs
+++ FriendOrganizer/FriendOrganizer.UI/View/Services/MessageDialogService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,21 +9,27 @@ using System.Windows;
 
 namespace FriendOrganizer.UI.View.Services
 {
-    public class MessageDialogService : IMessageDialogService
+  public class MessageDialogService : IMessageDialogService
+  {
+    private MetroWindow MetroWindow => (MetroWindow)App.Current.MainWindow;
+    public async Task<MessageDialogResult> ShowOkCancelDialogAsync(string text,string title)
     {
-        public MessageDialogResult ShowOkCancelDialog(string text, string title)
-        {
-            var result = MessageBox.Show(text, title, MessageBoxButton.OKCancel);
-            return result == MessageBoxResult.OK
-                ? MessageDialogResult.OK
-                : MessageDialogResult.Cancel;
-        }
+     var result=
+       await MetroWindow.ShowMessageAsync(title, text, MessageDialogStyle.AffirmativeAndNegative);
+      
+      return result == MahApps.Metro.Controls.Dialogs.MessageDialogResult.Affirmative
+        ? MessageDialogResult.OK
+        : MessageDialogResult.Cancel;
+    }
+    public async Task ShowInfoDialogAsync(string text)
+    {
+      await MetroWindow.ShowMessageAsync("Info", text);
+    }
 
-        
-    }
-    public enum MessageDialogResult
-    {
-        OK,
-        Cancel
-    }
+  }
+  public enum MessageDialogResult
+  {
+    OK,
+    Cancel
+  }
 }
